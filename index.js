6619d29c0923c91998edfc250971de43b02d072d
sujan321-oss <khumapokharel2076@gmail.com>
2025-02-23 19:44:01 +0530

    update the file with new env

--- index.js
+++ index.js
@@ -2,13 +2,16 @@ const express = require("express");
 
 const app = express();
 
+const data = process.env.NODE_ENV || "this is manual"
+
 app.get("/", (req, res) => {
-    res.json({ "msg": "Deploying the application" });
+    res.json({ "msg": `emssage from the server ${data} ` });
 });
 
 // Use process.env.PORT or default to 8001 (for local testing)
 const PORT = process.env.PORT || 8001; 
 
+
 app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`);
+    console.log(`Listening on port ${PORT} `);
 });
